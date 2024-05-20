f012ce10eb851d576fb4b5b76e1e1b931c2bce00
Whazt <walddt05@gmail.com>
2024-05-19 20:45:22 -0600

    Solucion de rutas

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -1,11 +1,24 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { ArrowLeftIcon } from "@heroicons/react/24/solid";
+
+
 
 const BackButton = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleBackClick = () => {
+    if (location.key === 'default') {
+      
+      navigate('/formulas');
+    } else {
+      
+      navigate(-1);
+    }
+  };
 
   return (
-    <button onClick={() => navigate(-1)}><ArrowLeftIcon className="w-6 h-6 m-2"/></button>
+    <button onClick={handleBackClick}><ArrowLeftIcon className="w-6 h-6 m-2"/></button>
   );
 };
 
